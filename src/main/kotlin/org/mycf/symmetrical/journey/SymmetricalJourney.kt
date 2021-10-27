806a78f0d483afb25a35bf4fbc9d7a1b1ed9b4fb
rlDruDo <ichundeli@gmail.com>
2021-10-28 00:27:43 +0200

    init commit

---
+++ src/main/kotlin/org/mycf/symmetrical/journey/SymmetricalJourney.kt
@@ -0,0 +1,15 @@
+package org.mycf.symmetrical.journey
+
+import net.fabricmc.api.ModInitializer
+
+
+@Suppress("UNUSED")
+object SymmetricalJourney: ModInitializer {
+    private const val MOD_ID = "symmetrical-journey"
+
+
+
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
